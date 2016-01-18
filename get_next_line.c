a27740ba343c3ebce2c6fb9b56ce8592ca407c2f
Victor BRAEKE <vbraeke@e2r4p12.42.fr>
2016-01-18 15:56:12 +0100

    majgnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,43 @@
 /*   By: vbraeke <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/16 13:42:27 by vbraeke           #+#    #+#             */
-/*   Updated: 2016/01/17 19:43:37 by vbraeke          ###   ########.fr       */
+/*   Updated: 2016/01/18 15:55:53 by vbraeke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int		check_line(char *buf)
+{
+	int		i;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+char	get_my_line(*buf)
+{
+
+}
 
 char	*ft_realloc(char *buf)
 {
 	char *buf2;
-	if (buff == 0)
+	if (buf == 0)
 	{
 		if ((buf2 = malloc(sizeof(char) * (BUFF_SIZE + 1))) == 0)
 			return (NULL);
 	}
 	else 
 	{
-		if ((buf2 = malloc(sizeof(char) * ft_strlen(buff) + BUFF_SIZE + 1)) == 0)
+		if ((buf2 = malloc(sizeof(char) * ft_strlen(buf) + BUFF_SIZE + 1)) == 0)
 			return (NULL);
-		ft_strcpy(buf2, buff);
-		free(buff);
+		ft_strcpy(buf2, buf);
+		free(buf);
 	}
 	return (buf2);
 }
@@ -34,23 +50,16 @@ char	*ft_realloc(char *buf)
 int		get_next_line(int const fd, char **line)
 {
 	char		*str;
-	static char	buff[BUFF_SIZE + 1];
+	static char	buf[BUFF_SIZE + 1];
 	int			ret;
 	static char	*save;
+	int			k;
 
-	j = 0;
-	if ((str = malloc(sizeof(char) * (BUF_SIZE + 1))) == NULL)
-		    return (NULL);
-	str[0] = '\0';
-	while (str)
+	if (*line != NULL)
 	{
-		if (buff[i] == '\0')
-			str = ft_realloc(str);
-		if (!(fd  = open(fd, O_RDONLY)) || fd < 1)
-			return (-1);
-		if (!(ret = read(fd, line, BUFFER)) || ret < 1)
-			return (-1);
-		close(fd);
-		return (1);
+		while (read(fd, buf, BUFF_SIZE))
+		{
+			k = checkline(buf);
+			save = ft_realloc(buf);
+		}
 	}
-}
